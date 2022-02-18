d8de96d7ba8b36ff10d3bbeac526e7c3d5b66d7d
Luis Sandoval <luismsandoval96@gmail.com>
2022-02-17 16:40:13 -0800

    added css to my page and added 3 img files

--- js/app.js
+++ js/app.js
@@ -14,9 +14,9 @@ it reverses the value
 */
 
 let username = prompt('What is your name?');
-alert('Hi ' + username + '!');
+alert('Hi ' + username + '! ' + 'Welcome to my site!');
 
-alert('Take this quiz to see if you really know me!');
+alert('Take this quiz to see if you really know me! Please answer with \'yes\' or \'no\'');
 
 let game = prompt('Do I play video games?').toLowerCase();
 
@@ -38,22 +38,22 @@ if (zodiac === 'yes' || zodiac === 'y') {
   /*console.log*/alert('You need to answer with a \'yes\' or a \'no\'');
 }
 
-let weights = prompt('Am I an anime fan?').toLowerCase();
+let anime = prompt('Am I an anime fan?').toLowerCase();
 
-if (weights === 'yes' || weights === 'y') {
+if (anime === 'yes' || anime === 'y') {
   /*console.log*/alert('Duh! uWu where\'s my waifu');
-} else if (weights === 'no' || weights === 'n') {
+} else if (anime === 'no' || anime === 'n') {
   /*console.log*/alert('Dude WHAT! I love the waifus');
 } else {
   /*console.log*/alert('You need to answer with a \'yes\' or a \'no\'');
 }
 
-let height = prompt('Am I tall?').toLowerCase();
+let pet = prompt('Do I have dogs?').toLowerCase();
 
-if (height === 'yes' || height === 'y') {
-  /*console.log*/alert('Dang right I am tall. Thank you for the compliment!');
-} else if (height === 'no' || height === 'n') {
-  /*console.log*/alert('Not tall relative to what???');
+if (pet === 'yes' || pet === 'y') {
+  /*console.log*/alert('Correct! I have two girls, Nala and Betty.');
+} else if (pet === 'no' || pet === 'n') {
+  /*console.log*/alert('Wrong again. Dude you should know I\'m a dog person.');
 } else {
   /*console.log*/alert('You need to answer with a \'yes\' or a \'no\'');
 }

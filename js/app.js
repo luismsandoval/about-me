f62d7d238d9c6628ccd11e181cd485d20c7294e6
Jeramie Green <93801788+JeramieGreen@users.noreply.github.com>
2022-02-22 21:16:02 -0800

    Added function to question seven

--- js/app.js
+++ js/app.js
@@ -88,34 +88,34 @@ questionFour();
 function questionFive(){
 
 
-let home = prompt('Am I from San Diego, CA?').toLowerCase();
+  let home = prompt('Am I from San Diego, CA?').toLowerCase();
 
-if (home === 'yes' || home === 'y') {
+  if (home === 'yes' || home === 'y') {
   /*console.log*/alert('Nope! But it was my home for 4 years and we miss it. :(');
-} else if (home === 'no' || home === 'n') {
+  } else if (home === 'no' || home === 'n') {
   /*console.log*/alert('Correct! I\'m from Wichita, Kansas. The air capital of the world!');
-  score++;
-} else {
+    score++;
+  } else {
   /*console.log*/alert('You need to answer with a \'yes\' or a \'no\'');
-}
+  }
 }
 questionFive();
 
 function questionSix(){
-let attempts = 3;
+  let attempts = 3;
 
-for (let i = 0; i < 4; i++) {
-  let number = parseInt(prompt('guess a number from 1-10'));
-  if (number === 7) {
+  for (let i = 0; i < 4; i++) {
+    let number = parseInt(prompt('guess a number from 1-10'));
+    if (number === 7) {
     /*console.log*/alert('That is right!');
-    score++;
-    break;
-  } if (number < 7) {
+      score++;
+      break;
+    } if (number < 7) {
     /*console.log*/alert(`too low, try again! You have ${attempts--} attempts remaining`);
-  } if (number > 7) {
+    } if (number > 7) {
     /*console.log*/alert(`too high, try again! You have ${attempts--} attempts remaining`);
-  }
-} /*console.log*/alert('the number was 7');
+    }
+  } /*console.log*/alert('the number was 7');
 }
 questionSix();
 
@@ -134,80 +134,84 @@ questionSix();
 //     console.log(' nope');
 //   }
 // }
+function questionSeven(){
 //                 0        1         2             3              4              5             etc
-let favorite = ['pizza', 'taco', 'cheeseburger', 'lasagna', 'baconeggncheese', 'burrito', 'chickenparm', 'padthai', 'friedchicken', 'pho'];
+  let favorite = ['pizza', 'taco', 'cheeseburger', 'lasagna', 'baconeggncheese', 'burrito', 'chickenparm', 'padthai', 'friedchicken', 'pho'];
 
-console.log(favorite.length);
+  console.log(favorite.length);
 
-let moreAttempts = 6;
+  let moreAttempts = 6;
 
-// for (let i = 0; i < 6; i++) {
-//   //for (let j = 0; j < favorite.length; j++) {
-//   let userResponse = prompt(`guess one of my top 10 favorite foods! You have ${moreAttempts - i} attempts!`).toLowerCase();
-//   if (/*favorite.includes(userResponse)*/userResponse === favorite[0] /*|| favorite[1] || favorite[2] || favorite[3] || favorite[4] || favorite[5] || favorite[6] || favorite[7] || favorite[8] || favorite[9]*/) {
-//     console.log('That is correct');
-//     score++;
-//     break;
-//   } if (userResponse === favorite[1]) {
-//     console.log('That is correct');
-//     score++;
-//     break;
-//   } if (userResponse === favorite[2]) {
-//     console.log('That is correct');
-//     score++;
-//     break;
-//   } if (userResponse === favorite[3]) {
-//     console.log('That is correct');
-//     score++;
-//     break;
-//   } if (userResponse === favorite[4]) {
-//     console.log('That is correct');
-//     score++;
-//     break;
-//   } if (userResponse === favorite[5]) {
-//     console.log('That is correct');
-//     score++;
-//     break;
-//   } if (userResponse === favorite[6]) {
-//     console.log('That is correct');
-//     score++;
-//     break;
-//   } if (userResponse === favorite[7]) {
-//     console.log('That is correct');
-//     score++;
-//     break;
-//   } if (userResponse === favorite[8]) {
-//     console.log('That is correct');
-//     score++;
-//     break;
-//   } if (userResponse === favorite[9]) {
-//     console.log('That is correct');
-//     score++;
-//     break;
-//   } else {
-//     console.log('That is incorrect');
-//   }
-//   //}
-//   console.log(`you have ${moreAttempts - j - 1} attempts remaining`);
-// }
-let answeredCorrectly = false;
+  // for (let i = 0; i < 6; i++) {
+  //   //for (let j = 0; j < favorite.length; j++) {
+  //   let userResponse = prompt(`guess one of my top 10 favorite foods! You have ${moreAttempts - i} attempts!`).toLowerCase();
+  //   if (/*favorite.includes(userResponse)*/userResponse === favorite[0] /*|| favorite[1] || favorite[2] || favorite[3] || favorite[4] || favorite[5] || favorite[6] || favorite[7] || favorite[8] || favorite[9]*/) {
+  //     console.log('That is correct');
+  //     score++;
+  //     break;
+  //   } if (userResponse === favorite[1]) {
+  //     console.log('That is correct');
+  //     score++;
+  //     break;
+  //   } if (userResponse === favorite[2]) {
+  //     console.log('That is correct');
+  //     score++;
+  //     break;
+  //   } if (userResponse === favorite[3]) {
+  //     console.log('That is correct');
+  //     score++;
+  //     break;
+  //   } if (userResponse === favorite[4]) {
+  //     console.log('That is correct');
+  //     score++;
+  //     break;
+  //   } if (userResponse === favorite[5]) {
+  //     console.log('That is correct');
+  //     score++;
+  //     break;
+  //   } if (userResponse === favorite[6]) {
+  //     console.log('That is correct');
+  //     score++;
+  //     break;
+  //   } if (userResponse === favorite[7]) {
+  //     console.log('That is correct');
+  //     score++;
+  //     break;
+  //   } if (userResponse === favorite[8]) {
+  //     console.log('That is correct');
+  //     score++;
+  //     break;
+  //   } if (userResponse === favorite[9]) {
+  //     console.log('That is correct');
+  //     score++;
+  //     break;
+  //   } else {
+  //     console.log('That is incorrect');
+  //   }
+  //   //}
+  //   console.log(`you have ${moreAttempts - j - 1} attempts remaining`);
+  // }
+  let answeredCorrectly = false;
 
-for (let i = 0; i < 6; i++) {
-  let userResponse = prompt(`guess one of my top 10 favorite foods! You have ${moreAttempts - i} attempts!`).toLowerCase();
-  for (let j = 0; j < favorite.length; j++) {
-    if (userResponse === favorite[j]) {
-      console.log('That is correct!');
-      answeredCorrectly = true;
-      score++;
-      break;
-    } if (userResponse !== favorite[j]) {
-      console.log('That is incorrect!');
+  for (let i = 0; i < 6; i++) {
+    let userResponse = prompt(`guess one of my top 10 favorite foods! You have ${moreAttempts - i} attempts!`).toLowerCase();
+    for (let j = 0; j < favorite.length; j++) {
+      if (userResponse === favorite[j]) {
+        console.log('That is correct!');
+        answeredCorrectly = true;
+        score++;
+        break;
+      } if (userResponse !== favorite[j]) {
+        console.log('That is incorrect!');
+        break;
+      }
+    } if (answeredCorrectly) {
       break;
     }
-  } if (answeredCorrectly) {
-    break;
   }
 }
+
+questionSeven();
 
 alert('You did pretty good ' + username + '! ' + 'Maybe you are a good friend!');
 alert('Nice Job, you got ' + score + ' out of 7');
